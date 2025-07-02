7db0350141e63a50c56e063c77bf3da1ddb20e14
Kadu <cadumagus@gmail.com>
2025-07-02 15:10:08 -0300

    added main and stats.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from stats import count_words
+from stats import count_words, count_letters
 
 def get_book_text(filepath: str):
     with open(filepath) as book:
         text = book.read()
 
     words_num = count_words(text)
-    result = f"{words_num} words found in the document"
-    return result
+    char_count = count_letters(text)
+    result = f"{words_num} words found in the document\n"
+    return result, char_count
 
 def main():
     print(get_book_text("books/frankenstein.txt"))
--- stats.py
+++ stats.py
@@ -3,3 +3,16 @@ def count_words(text: str):
     alpha = len(words)
 
     return alpha
+
+def count_letters(text: str):
+    lower = text.lower()
+    let = {}
+
+    for letter in lower:
+        if letter.isalpha() and not letter in let:
+            let[letter] = 1
+        elif letter in let:
+            let[letter] += 1
+    return let
+
+    
