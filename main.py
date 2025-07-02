c03c0ceb579171dc85365793cca0c78764659649
Kadu <cadumagus@gmail.com>
2025-07-02 12:04:49 -0300

    updated main.py and .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from stats import count_words
+
+def get_book_text(filepath: str):
+    with open(filepath) as book:
+        text = book.read()
+
+    words_num = count_words(text)
+    result = f"{words_num} words found in the document"
+    return result
+
 def main():
-    print("Hello from bookbot!")
+    print(get_book_text("books/frankenstein.txt"))
 
 
 if __name__ == "__main__":
